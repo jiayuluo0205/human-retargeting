220f5de3197910b8fd64b93ff2436fe1be3beabd
BorisGuo <30684004+BorisGuo6@users.noreply.github.com>
2024-11-06 22:50:58 +0800

    add new

---
+++ utils/CM_YD_SDK_Lib_2022/CM_YD_SDK_Lib_2022/Lib/CM_YD_SDK.h
@@ -0,0 +1,138 @@
+﻿#pragma once
+#include <winsock2.h>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#pragma comment(lib, "ws2_32.lib")
+#include "Lib/nlohmann/json.hpp"
+
+#ifndef __CM_YD_Glove_H__
+#define __CM_YD_Glove_H__
+
+#define YDGlove_SDK __declspec( dllexport )
+
+#endif
+
+using json = nlohmann::json;
+using namespace std;
+
+class GloveSDK;
+const array<string, 5> FingerNames = { "Thumb", "Index", "Middle", "Ring", "Pinky" };
+const array<string, 4> BonePartNames = { "Metacarpal", "Proximal", "Intermediate", "Distal" };
+const array<string, 2> ThumbBonePartNames = { "Metacarpal", "Proximal" };
+
+static mutex data_mutex;
+static condition_variable data_cond;
+static atomic<bool> data_available(false);
+static mutex control_console_mux;
+
+void OnlyPrintFrame();
+
+/*
+* 数据结构
+* Data Structure
+*/
+struct HandDriverData
+{
+	int DeviceID = 0;
+	int FrameIndex = 0;
+	string DeviceName;
+	int CalibrationStatus;
+	int Battery;
+
+	/*
+	* 15个指节 每个指节有4旋转项数据 ，新增一个 imu 数据 位于第16个位置
+	* 15 knuckles, each knuckle has 4 rotation items, add an imu data at the 16th position
+	*/
+	array<array<float, 4>, 16> JointDatas; 
+	int Count = 0;
+};
+
+/*
+* 此结构体存放了1双手套的数据 分为L和R
+* This structure stores the data of a pair of gloves, divided into L and R
+*/
+struct PairGloveData 
+{
+	HandDriverData HandData_L;
+	HandDriverData HandData_R;
+	bool bPrintData;
+};
+void print_control_thread(GloveSDK* glove_sdk);
+
+/*
+* 回调函数类型
+* Callback function type
+*/
+using GloveCallBack = function<void(GloveSDK*)>; 
+
+class YDGlove_SDK GloveSDK
+{
+public:
+	/*
+	* 初始化 socket 和线程
+	* Init Soicket and thread
+	*/
+	bool Initialize(const char* udp_ip, unsigned short udp_port);
+
+	/*
+	* 关闭线程，清理socket
+	* close thread, clear socket
+	*/
+	void Shutdown();
+
+	/*
+	* 是否要打印数据到控制台
+	* Do you want to print data on the console.
+	*/
+	void EnablePrintData(int DeviceID, bool bEnable);
+
+	/*
+	* 2组“1双手套”数据，因为现在软件里一台电脑只能发两幅，后面迭代版本会增多
+	* 2 sets of "1 pair of gloves" data, because the current software can only send two pictures per computer, and there will be more in the subsequent iterations
+	* GLoveData[0]就是ID为0的设备
+	* GLoveData[0] It is the device with ID 0
+	*/
+	array<PairGloveData, 8> GloveData;//需要几双手套修改w为几即可 //Modify w to the number of pairs of gloves you need
+
+	/*
+	* 注册回调函数
+	* Registering callback function
+	*/
+	void RegisterGloveCallBack(GloveCallBack callback);
+
+	/*
+	* 显示手套数据（静态函数）
+	* Display glove data (static function)
+	*/
+	static void show_data(HandDriverData& GloveData);
+
+	/*
+	* 四元数转欧拉角
+	* Quaternion to Euler Angle
+	*/
+	void toEulerAngle(const double x, const double y, const double z, const double w, double& roll, double& pitch, double& yaw);
+
+	/*
+	* 弧度到角度
+	* Euler angle to rotation angle
+	*/
+	double RadToAngle(double rad);
+
+private:
+	static const int BUFFER_SIZE = 80000; //缓存大小 //Cache size
+	SOCKET m_udp_socket;
+	atomic<bool> m_running;
+	thread m_receive_thread;
+	void ReceiveThread();
+	void ParseJson(char* json_buffer, int length);
+
+	void TrySaveData(HandDriverData& GloveData, nlohmann::json received_data);
+
+	void IniData();	
+
+	GloveCallBack dataUpdateCallback;
+};
+
+
+
---
+++ utils/CM_YD_SDK_Lib_2022/CM_YD_SDK_Lib_2022/Mian.cpp
@@ -0,0 +1,102 @@
+﻿// CM_YD_SDK_Lib_2022.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Lib/CM_YD_SDK.h"
+#pragma comment(lib,"Lib/CM_YD_SDK.lib")
+
+double roll, pitch, yaw;
+
+/*
+* 定义了一个函数用于注册回调
+* The cache size defines a function for registering a callback
+*/
+void OnNewGloveData(GloveSDK* glovePtr)
+{
+	/*
+	* 这个函数将在接受线程中被调用
+	* This function will be called in the receiving thread
+	*
+	* 打印手数据
+	* Print gloveData
+	*/
+	auto a = glovePtr->GloveData[0].HandData_L.JointDatas[6];//取第6个数据  ////Get the 6th data
+
+	cout << "LeftRingProximal： " << a[1] << endl;//打印四元数的第二个数 //Print the second number of quaternions
+
+	glovePtr->toEulerAngle(a[0], a[1], a[2], a[3], roll, pitch, yaw);//四元数转欧拉角,注意 IMU 的数据是 WXYZ 顺序  //Convert quaternions to Euler angles, note that IMU data is in WXYZ order
+
+	cout << "roll:" << glovePtr->RadToAngle(roll) << " pitch:" << glovePtr->RadToAngle(pitch) << " yaw:" << glovePtr->RadToAngle(yaw) << endl;//打印角度  //Print angle
+
+}
+
+int main()
+{
+	/*
+	* 创建一个 GloveSDK 的实例
+	* Create an instance of GloveSDK
+	*/
+	GloveSDK glove_sdk;
+
+	/*
+	* 注册回调
+	* Registering callback function
+	*/
+	glove_sdk.RegisterGloveCallBack(OnNewGloveData);
+
+	/*
+	* 创建Socket并检测是否成功，成功则接受数据并解析
+	* Create a Socket and check if it is successful. If successful, receive and parse the data
+	*/
+	if (!glove_sdk.Initialize("127.0.0.1", 7777))
+	{
+		cerr << "Failed to initialize GloveSDK." << endl;
+		return -1;
+	}
+
+	/*
+	* 开启打印线程，打印glove_sdk的数据
+	* Start the print thread and print the glove_sdk data
+	*/
+	//thread print_th(print_control_thread, &glove_sdk);
+
+	/*
+	* 调用后只打印帧数
+	* After calling, only the number of frames is printed
+	*/
+	while (true)
+	{
+		int a;
+		cin >> a;
+
+		switch (a)
+		{
+
+		case 1:
+			OnlyPrintFrame();
+			break;
+
+		case 0:
+			glove_sdk.Shutdown();
+			printf("Shutdown");
+			break;
+		}
+	}
+
+	/*
+	* 等待输入 保存程序
+	* Waiting for input, Save program
+	*/
+	//getchar();
+
+	/*
+	* 关闭清理socket
+	* close and clean up the socket
+	*/
+	glove_sdk.Shutdown();
+	printf("Shutdown");
+
+
+
+	return 0;
+}
