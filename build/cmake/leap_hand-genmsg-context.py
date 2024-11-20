12be1eb7be88f9aeca890c751a7fe98c05c50239
BorisGuo <borisguo6@gmail.com>
2024-11-20 15:45:44 +0800

    update

---
+++ build/cmake/leap_hand-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = ""
+services_str = "/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_velocity.srv;/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_position.srv;/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_effort.srv"
+pkg_name = "leap_hand"
+dependencies_str = "std_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "std_msgs;/opt/ros/noetic/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/home/boris/anaconda3/bin/python3"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/noetic/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
---
+++ build/devel/share/leap_hand/cmake/leap_hand-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(leap_hand_MESSAGE_FILES "")
+set(leap_hand_SERVICE_FILES "/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_velocity.srv;/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_position.srv;/home/boris/workspace/human-retargeting/3rdparty/LEAP_Hand_API/ros_module/srv/leap_effort.srv")
