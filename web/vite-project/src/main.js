0c52b5ec10c3e1bebe0ca7267af3722b55c671e9
BorisGuo <borisguo6@gmail.com>
2024-12-03 13:57:57 +0800

    update

--- web/vite-project/src/main.js
+++ web/vite-project/src/main.js
@@ -1,153 +1,285 @@
-import {WebXRButton} from '../js/util/webxr-button.js';
-import {Scene} from '../js/render/scenes/scene.js';
-import {Renderer, createWebGLContext} from '../js/render/core/renderer.js';
-import {SkyboxNode} from '../js/render/nodes/skybox.js';
-import {InlineViewerHelper} from '../js/util/inline-viewer-helper.js';
-import {Gltf2Node} from '../js/render/nodes/gltf2.js';
-import {QueryArgs} from '../js/util/query-args.js';
-
-// If requested, use the polyfill to provide support for mobile devices
-// and devices which only support WebVR.
+// main.js
+import { WebXRButton } from '../js/util/webxr-button.js';
+import { Scene } from '../js/render/scenes/scene.js';
+import { Node } from '../js/render/core/node.js';
+import { Gltf2Node } from '../js/render/nodes/gltf2.js';
+import { Renderer, createWebGLContext } from '../js/render/core/renderer.js';
+import { BoxBuilder } from '../js/render/geometry/box-builder.js';
+import { PbrMaterial } from '../js/render/materials/pbr.js';
+import { QueryArgs } from '../js/util/query-args.js';
+import { mat4 } from '../js/render/math/gl-matrix.js';
+import { vec3 } from '../js/render/math/gl-matrix.js';
+import { Ray } from '../js/render/math/ray.js';
+import { fetchProfile } from 'https://cdn.jsdelivr.net/npm/@webxr-input-profiles/motion-controllers@1.0/dist/motion-controllers.module.js';
+const DEFAULT_PROFILES_PATH = 'https://cdn.jsdelivr.net/npm/@webxr-input-profiles/assets@1.0/dist/profiles';
 import WebXRPolyfill from '../js/third-party/webxr-polyfill/build/webxr-polyfill.module.js';
-if (QueryArgs.getBool('usePolyfill', true)) {
-let polyfill = new WebXRPolyfill();
-}
+import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 
-// XR globals.
+// 初始化全局变量
 let xrButton = null;
-let xrImmersiveRefSpace = null;
-let inlineViewerHelper = null;
+let xrRefSpace = null;
+let isAR = true;
+let radii = new Float32Array(25);
+let positions = new Float32Array(16 * 25);
+let boxes_left = [];
+let boxes_right = [];
+let boxes_none = [];
+let tracked_boxes_left = [];
+let tracked_boxes_right = [];
+let tracked_boxes_none = [];
+let boxes = { input_left: boxes_left, input_right: boxes_right, input_none: boxes_none, tracked_left: tracked_boxes_left, tracked_right: tracked_boxes_right, tracked_none: tracked_boxes_none };
+let indexFingerBoxes = { input_left: null, input_right: null, tracked_left: null, tracked_right: null };
+const defaultBoxColor = { r: 0.5, g: 0.5, b: 0.5 };
+const leftBoxColor = { r: 1, g: 0, b: 1 };
+const rightBoxColor = { r: 0, g: 1, b: 1 };
 
-// WebGL scene globals.
+let interactionBox = null;
+let leftInteractionBox = null;
+let rightInteractionBox = null;
+
 let gl = null;
 let renderer = null;
 let scene = new Scene();
-let solarSystem = new Gltf2Node({url: '../media/gltf/sunflower/sunflower.gltf'});
-// The solar system is big (citation needed). Scale it down so that users
-// can move around the planets more easily.
+let usePolyfill = true;
+
+let solarSystem = new Gltf2Node({url: '../media/gltf/space/space.gltf'});
+// let solarSystem = new Gltf2Node({url: '../media/stl/xarm6/base.gltf'});
 solarSystem.scale = [0.1, 0.1, 0.1];
 scene.addNode(solarSystem);
-// Still adding a skybox, but only for the benefit of the inline view.
-let skybox = new SkyboxNode({url: '../media/textures/milky-way-4k.png'});
-scene.addNode(skybox);
+
+if (QueryArgs.getBool('usePolyfill', true)) {
+  new WebXRPolyfill({ debug: false });
+}
 
 scene.enableStats(false); // 禁用 FPS 计数器
 
+
+function createBoxPrimitive(r, g, b) {
+  let boxBuilder = new BoxBuilder();
+  boxBuilder.pushCube([0, 0, 0], 1);
+  let boxPrimitive = boxBuilder.finishPrimitive(renderer);
+  let boxMaterial = new PbrMaterial();
+  boxMaterial.baseColorFactor.value = [r, g, b, 1];
+  return renderer.createRenderPrimitive(boxPrimitive, boxMaterial);
+}
+
+function addBox(x, y, z, r, g, b, offset) {
+  let boxRenderPrimitive = createBoxPrimitive(r, g, b);
+  let boxNode = new Node();
+  boxNode.addRenderPrimitive(boxRenderPrimitive);
+  boxNode.selectable = true;
+  return boxNode;
+}
+
+function initHands() {
+  for (const box of boxes_left) scene.removeNode(box);
+  for (const box of boxes_right) scene.removeNode(box);
+  boxes_left = [];
+  boxes_right = [];
+  boxes = { input_left: boxes_left, input_right: boxes_right, input_none: boxes_none, tracked_left: tracked_boxes_left, tracked_right: tracked_boxes_right, tracked_none: tracked_boxes_none };
+
+  if (typeof XRHand !== 'undefined') {
+    for (let i = 0; i <= 24; i++) {
+      const r = .6 + Math.random() * .4;
+      const g = .6 + Math.random() * .4;
+      const b = .6 + Math.random() * .4;
+      boxes_left.push(addBox(0, 0, 0, r, g, b));
+      boxes_right.push(addBox(0, 0, 0, r, g, b));
+      tracked_boxes_left.push(addBox(0, 0, 0, r, g, b));
+      tracked_boxes_right.push(addBox(0, 0, 0, r, g, b));
+    }
+  }
+
+  if (indexFingerBoxes.input_left) scene.removeNode(indexFingerBoxes.left);
+  if (indexFingerBoxes.input_right) scene.removeNode(indexFingerBoxes.input_right);
+  if (indexFingerBoxes.tracked_left) scene.removeNode(indexFingerBoxes.tracked_left);
+  if (indexFingerBoxes.tracked_right) scene.removeNode(indexFingerBoxes.tracked_right);
+
+  indexFingerBoxes.input_left = addBox(0, 0, 0, leftBoxColor.r, leftBoxColor.g, leftBoxColor.b);
+  indexFingerBoxes.input_right = addBox(0, 0, 0, rightBoxColor.r, rightBoxColor.g, rightBoxColor.b);
+  indexFingerBoxes.tracked_left = addBox(0, 0, 0, leftBoxColor.r, leftBoxColor.g, leftBoxColor.b);
+  indexFingerBoxes.tracked_right = addBox(0, 0, 0, rightBoxColor.r, rightBoxColor.g, rightBoxColor.b);
+}
+
 function initXR() {
-xrButton = new WebXRButton({
+  xrButton = new WebXRButton({
     onRequestSession: onRequestSession,
     onEndSession: onEndSession,
     textEnterXRTitle: "START AR",
     textXRNotFoundTitle: "AR NOT FOUND",
-    textExitXRTitle: "EXIT  AR",
-});
-document.querySelector('header').appendChild(xrButton.domElement);
+    textExitXRTitle: "EXIT AR",
+  });
+  document.querySelector('header').appendChild(xrButton.domElement);
 
-if (navigator.xr) {
-    // Checks to ensure that 'immersive-ar' mode is available, and only
-    // enables the button if so.
+  if (navigator.xr) {
     navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
-    xrButton.enabled = supported;
+      if (supported) xrButton.enabled = supported;
+      else xrButton.enabled = false;
     });
-
-    navigator.xr.requestSession('inline').then(onSessionStarted);
-}
+    // navigator.xr.requestSession('inline').then(onSessionStarted);
+  }
 }
 
 function onRequestSession() {
-// Requests an 'immersive-ar' session, which ensures that the users
-// environment will be visible either via video passthrough or a
-// transparent display. This may be presented either in a headset or
-// fullscreen on a mobile device.
-return navigator.xr.requestSession('immersive-ar')
-    .then((session) => {
-        xrButton.setSession(session);
-        session.isImmersive = true;
-        onSessionStarted(session);
-    });
+  return navigator.xr.requestSession('immersive-ar', { optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking'] }).then(onSessionStarted);
 }
 
 function initGL() {
-if (gl)
+  if (gl)
     return;
 
-gl = createWebGLContext({
+  gl = createWebGLContext({
     xrCompatible: true
-});
-document.body.appendChild(gl.canvas);
+  });
+  document.body.appendChild(gl.canvas);
 
-function onResize() {
+  function onResize() {
     gl.canvas.width = gl.canvas.clientWidth * window.devicePixelRatio;
     gl.canvas.height = gl.canvas.clientHeight * window.devicePixelRatio;
-}
-window.addEventListener('resize', onResize);
-onResize();
+  }
+  window.addEventListener('resize', onResize);
+  onResize();
 
-renderer = new Renderer(gl);
+  renderer = new Renderer(gl);
 
-scene.setRenderer(renderer);
+  scene.setRenderer(renderer);
 }
 
 function onSessionStarted(session) {
-session.addEventListener('end', onSessionEnded);
+  xrButton.setSession(session);
+  session.addEventListener('end', onSessionEnded);
+  session.addEventListener('inputsourceschange', onInputSourcesChange);
 
-if (session.isImmersive) {
-    // When in 'immersive-ar' mode don't draw an opaque background because
-    // we want the real world to show through.
-    skybox.visible = false;
-}
+  initGL();
 
-initGL();
+  initHands();
 
-session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
+  scene.setRenderer(renderer);
 
-let refSpaceType = session.isImmersive ? 'local' : 'viewer';
-session.requestReferenceSpace(refSpaceType).then((refSpace) => {
-    if (session.isImmersive) {
-    xrImmersiveRefSpace = refSpace;
+  session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
 
-    xrImmersiveRefSpace.addEventListener('reset', (evt) => {
-        if (evt.transform) {
-        // AR experiences typically should stay grounded to the real world.
-        // If there's a known origin shift, compensate for it here.
-        xrImmersiveRefSpace = xrImmersiveRefSpace.getOffsetReferenceSpace(evt.transform);
-        }
-    });
-    } else {
-    inlineViewerHelper = new InlineViewerHelper(gl.canvas, refSpace);
-    }
+  session.requestReferenceSpace('local').then((refSpace) => {
+    xrRefSpace = refSpace.getOffsetReferenceSpace(new XRRigidTransform({ x: 0, y: 0, z: 0 }));
     session.requestAnimationFrame(onXRFrame);
-});
+  });
 }
 
 function onEndSession(session) {
-session.end();
+  session.end();
 }
 
 function onSessionEnded(event) {
-if (event.session.isImmersive) {
-    xrButton.setSession(null);
-    // Turn the background back on when we go back to the inlive view.
-    skybox.visible = true;
-}
+  xrButton.setSession(null);
+  renderer = null;
 }
 
-// Called every time a XRSession requests that a new frame be drawn.
+function onInputSourcesChange(event) {
+  onSourcesChange(event, "input_");
+}
+
+function onSourcesChange(event, type) {
+  for (let inputSource of event.added) {
+    if (inputSource.targetRayMode == 'tracked-pointer') {
+      fetchProfile(inputSource, DEFAULT_PROFILES_PATH).then(({ profile, assetPath }) => {
+        scene.inputRenderer.setControllerMesh(new Gltf2Node({ url: assetPath }), inputSource.handedness, inputSource.profiles[0]);
+      });
+    }
+  }
+}
+
+function updateInputSources(session, frame, refSpace) {
+  updateSources(session, frame, refSpace, session.inputSources, "input_");
+}
+
+function updateSources(session, frame, refSpace, sources, type) {
+  if (session.visibilityState === 'visible-blurred') return;
+  for (let inputSource of sources) {
+    let hand_type = type + inputSource.handedness;
+    if (type == "input_") {
+      let targetRayPose = frame.getPose(inputSource.targetRaySpace, refSpace);
+      if (targetRayPose) {
+        let targetRay = new Ray(targetRayPose.transform);
+        let cursorDistance = 2.0;
+        let cursorPos = vec3.fromValues(
+          targetRay.origin.x,
+          targetRay.origin.y,
+          targetRay.origin.z
+        );
+        vec3.add(cursorPos, cursorPos, [
+          targetRay.direction.x * cursorDistance,
+          targetRay.direction.y * cursorDistance,
+          targetRay.direction.z * cursorDistance,
+        ]);
+        scene.inputRenderer.addCursor(cursorPos);
+      }
+    }
+
+    if (!inputSource.hand && inputSource.gripSpace) {
+      let gripPose = frame.getPose(inputSource.gripSpace, refSpace);
+      if (gripPose) {
+        scene.inputRenderer.addController(gripPose.transform.matrix, inputSource.handedness, inputSource.profiles[0]);
+      }
+    }
+
+    let offset = 0;
+    if (!inputSource.hand) {
+      for (const box of boxes[hand_type]) scene.removeNode(box);
+      scene.removeNode(indexFingerBoxes[hand_type]);
+      continue;
+    } else {
+      let pose = frame.getPose(inputSource.targetRaySpace, refSpace);
+      if (pose === undefined) continue;
+
+      if (!frame.fillJointRadii(inputSource.hand.values(), radii)) continue;
+      if (!frame.fillPoses(inputSource.hand.values(), refSpace, positions)) continue;
+
+      for (const box of boxes[hand_type]) {
+        scene.addNode(box);
+        let matrix = positions.slice(offset * 16, (offset + 1) * 16);
+        let jointRadius = radii[offset];
+        offset++;
+        mat4.getTranslation(box.translation, matrix);
+        mat4.getRotation(box.rotation, matrix);
+        box.scale = [jointRadius, jointRadius, jointRadius];
+      }
+
+      const indexFingerBox = indexFingerBoxes[hand_type];
+      scene.addNode(indexFingerBox);
+      let joint = inputSource.hand.get('index-finger-tip');
+      let jointPose = frame.getJointPose(joint, xrRefSpace);
+      if (jointPose) {
+        let matrix = jointPose.transform.matrix;
+        mat4.getTranslation(indexFingerBox.translation, matrix);
+        mat4.getRotation(indexFingerBox.rotation, matrix);
+        indexFingerBox.scale = [0.007, 0.007, 0.007];
+      }
+    }
+  }
+}
+
 function onXRFrame(t, frame) {
-let session = frame.session;
-let refSpace = session.isImmersive ?
-                    xrImmersiveRefSpace :
-                    inlineViewerHelper.referenceSpace;
-let pose = frame.getViewerPose(refSpace);
+  let session = frame.session;
 
-scene.startFrame();
+  scene.startFrame();
+  session.requestAnimationFrame(onXRFrame);
 
-session.requestAnimationFrame(onXRFrame);
+  updateInputSources(session, frame, xrRefSpace);
 
-scene.drawXRFrame(frame, pose);
+  let pose = frame.getViewerPose(xrRefSpace);
+  if (pose) {
+    let glLayer = session.renderState.baseLayer;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-scene.endFrame();
+    for (let view of pose.views) {
+      let viewport = glLayer.getViewport(view);
+      gl.viewport(viewport.x, viewport.y, viewport.width, viewport.height);
+      scene.draw(view.projectionMatrix, view.transform);
+    }
+  }
+
+  scene.endFrame();
 }
 
-// Start the XR application.
 initXR();
